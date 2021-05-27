f8a93ce80675597f3a6144ac8f297f7b04a3a759
meso_test <45484407+HellInsider@users.noreply.github.com>
2021-05-27 21:23:40 +0300

    Add files via upload

--- code/core/manager.cpp
+++ code/core/manager.cpp
@@ -44,12 +44,55 @@ void manager::OnButton(int butttonPressed)
     case BUTTON_DOWN:
         SetFloor(quint32(currentFloor - 1));
         break;
-    case BUTTON_DRAW_PATH:
+    case BUTTON_DRAW_PATH:                                                          //Here is big Костыль in whole case.
         path_data pd = window.GetPathWidget()->GetData();
-        vec start(pd.x1, pd.y1);
-        vec end(pd.x2, pd.y2);
-        graph->SetStart(start, quint32(pd.floor1)-1);
-        graph->SetEnd(end, quint32(pd.floor2)-1);
+
+
+        int shop_1_index = SearchShop(pd.shop_1);
+        int shop_2_index = SearchShop(pd.shop_2);
+
+        if(shop_1_index < 0 || shop_2_index < 0)
+        {
+            break;
+        }
+
+        point* exits = currentMap.GetInfo()->getExits();
+        vec start(exits[shop_1_index].x, exits[shop_1_index].y);
+        vec end(exits[shop_2_index].x, exits[shop_2_index].y);
+
+        int floor1 =-1, floor2 = -1;
+        QString shop_name;
+
+        for(int i = 0; i < 3; i++)
+        {
+            mapping = (*floorLayers)[i].GetShopsLr().GetShops();
+            for(auto it = mapping.begin(); it != mapping.end(); it++)
+            {
+                if(it.value() == shop_1_index)
+                {
+                    floor1 = i;
+                }
+
+                if(it.value() == shop_2_index)
+                {
+                    floor2 = i;
+                    shop_name = it.key();
+                }
+            }
+        }
+
+        if(floor1 < 0 || floor2 < 0)
+        {
+            break;
+        }
+
+
+        mapViewer->HighlightShop(shop_name);
+
+        //vec start(pd.x1, pd.y1);
+        //vec end(pd.x2, pd.y2);
+        graph->SetStart(start, quint32(floor1));
+        graph->SetEnd(end, quint32(floor2));
         graph->FindPath();
         break;
     }
